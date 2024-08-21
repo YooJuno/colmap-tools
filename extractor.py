f4dc84460c0be09d074d087023e44f1bf2f78c87
YooJuno <juno980220@naver.com>
2024-08-21 16:19:16 +0900

    masking 병합

--- extractor.py
+++ extractor.py
@@ -1,29 +1,27 @@
 import cv2
 import os
+import numpy as np
 
-# 동영상 파일 경로
+
 video_name = 'F1.mp4'
 input_fps = 30
 
 video_path = './video/'
 
-# 비디오 캡처 객체 생성
 cap = cv2.VideoCapture(video_path + video_name)
 
-
-# 프레임 번호 초기화
 frame_number = 0
 
 video_fps = round(cap.get(cv2.CAP_PROP_FPS))
+video_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+video_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 print('video fps :' , video_fps)
 
 output_folder = 'workspace/' + video_name.split('.')[0] + '_' + str(input_fps) + 'fps/dataset'
 
-# 저장할 이미지 경로
 os.makedirs(output_folder, exist_ok=True)
 
-# 동영상의 프레임을 하나씩 읽어서 이미지로 저장
 while True:
     ret, frame = cap.read()
     
@@ -36,8 +34,12 @@ while True:
         cv2.imwrite(frame_filename, frame)
     
     frame_number += 1
-
-# 캡처 객체 해제
+    
 cap.release()
+
+mask = np.zeros((video_height,video_width), dtype=np.uint8)
+mask[93:950, : ] = 255 # RoI
+
+cv2.imwrite(video_name.split('.')[0] + '_mask.png', mask)
 
 print(f'Extracted {frame_number} frames to "{output_folder}"')
